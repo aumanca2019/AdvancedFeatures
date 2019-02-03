b1943aa11527b1fd313892feeb7dc77e4f839fd1
aumanca2019 <ancalumi68@yahoo.com>
2019-02-04 00:10:31 +0200

    Upload 03 02

---
+++ src/main/java/Enums/MyEnum.java
@@ -0,0 +1,35 @@
+package Enums;
+
+public class MyEnum {
+
+    public enum LenghtUnit {
+        METER(1),
+        CENTIMETER(0.01),
+        FOOT(0.03),
+        INCH(0.025);
+
+        double value;
+        LenghtUnit (double value) {
+            this.value = value;
+        }
+
+        double convertToMetters() {
+            return value;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LenghtUnit[]lenghtUnits = LenghtUnit.values();
+        for(LenghtUnit lenghtUnit:lenghtUnits){
+            System.out.println(lenghtUnit);
+        }
+        System.out.println(LenghtUnit.FOOT.convertToMetters());
+        System.out.println(LenghtUnit.CENTIMETER.convertToMetters());
+    }
+
+
+
+
+
+}
